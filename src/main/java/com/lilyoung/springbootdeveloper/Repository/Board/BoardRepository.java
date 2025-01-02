aa90d97b4eb41e00d5c87f85d965a961156cf465
송용인 <thddyddls1@naver.com>
2025-01-02 22:36:03 +0900

    update

--- src/main/java/com/lilyoung/springbootdeveloper/Repository/Board/BoardRepository.java
+++ src/main/java/com/lilyoung/springbootdeveloper/Repository/Board/BoardRepository.java
@@ -2,7 +2,6 @@ package com.lilyoung.springbootdeveloper.Repository.Board;
 
 import com.lilyoung.springbootdeveloper.Entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 @Repository
